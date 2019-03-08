0fffcc6fc85d4bc535bad80ef03e4d1df9e71d71
Ian Norris <icstatic@gmail.com>
2019-03-08 23:30:49 +0000

    Bare bones app

---
+++ LocalSchemeHandlerFactory.cs
@@ -0,0 +1,55 @@
+﻿using CefSharp;
+using System;
+using System.IO;
+
+namespace Jar
+{
+	class LocalSchemeHandlerFactory : ISchemeHandlerFactory
+	{
+		public IResourceHandler Create(IBrowser browser, IFrame frame, string schemeName, IRequest request)
+		{
+			Uri u = new Uri(request.Url);
+			String Base = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "content");
+			String file = Path.Combine(Base, u.AbsolutePath.Substring(1));
+
+			if (u.AbsolutePath.EndsWith("/"))
+			{
+				file += "\\index.html";
+			}
+
+			if (File.Exists(file))
+			{
+				Byte[] bytes = File.ReadAllBytes(file);
+				var ResponseStream = new MemoryStream(bytes);
+				var Type = "";
+				switch (Path.GetExtension(file))
+				{
+					case ".html":
+						Type = "text/html";
+						break;
+					case ".js":
+						Type = "text/javascript";
+						break;
+					case ".css":
+						Type = "text/css";
+						break;
+					case ".png":
+						Type = "image/png";
+						break;
+					case ".appcache":
+					case ".manifest":
+						Type = "text/cache-manifest";
+						break;
+					default:
+						Type = "application/octet-stream";
+						break;
+				}
+				return ResourceHandler.FromStream(ResponseStream, Type, true);
+			}
+			else
+			{
+				return ResourceHandler.ForErrorMessage("Not found", System.Net.HttpStatusCode.NotFound);
+			}
+		}
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jar
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			var Settings = new CefSettings();
+			Settings.RegisterScheme(
+				new CefCustomScheme
+				{
+					SchemeName = "local",
+					SchemeHandlerFactory = new LocalSchemeHandlerFactory()
+				}
+			);
+
+			Cef.Initialize(Settings);
+
+			InitializeComponent();
+		}
+
+		[Conditional("DEBUG")]
+		public void ShowDevTools()
+		{
+			m_browser.ShowDevTools();
+		}
+
+		private void m_browser_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if ((bool)e.NewValue)
+			{
+				m_browser.Load($@"local://index.html");
+
+				ShowDevTools();
+			}
+		}
+	}
+}
