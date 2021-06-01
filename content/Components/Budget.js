6ee79b6761a22a7be870d57b157d181fc42f6e4f
Ian Norris <icstatic@gmail.com>
2021-06-02 00:02:43 +0100

    Budget date header test

---
+++ content/Components/Budget.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+Vue.component('jar-budget', {
+	template: '#BudgetTemplate',
+	data: function () {
+		return {
+			earliestBudgetMonth: '2019-03-03 00:00',
+			lastBudgetMonth: '2020-09-03 00:00',
+			activeMonth: '2020-07-01 00:00',
+			year: 2020,
+		};
+	},
+	created: async function () {
+		
+	},
+	methods: {
+		getShortMonth: function (month) {
+			return month.format('MMM YYYY');
+		}
+	},
+	computed: {
+		months: function () {
+			var monthsOut = [];
+			var thisMonth = moment(`${this.year+1}-12-31 00:00`).startOf('year').subtract(12, 'months');
+			for (var months = 0; months < 12; months++) {
+				monthsOut.push({
+					month: thisMonth,
+					shortMonth: this.getShortMonth(thisMonth),
+					enabled: thisMonth.isBetween(moment(this.earliestBudgetMonth).startOf('month'), moment(this.lastBudgetMonth).startOf('month'), undefined, '[]'),
+					active: thisMonth.isSame(moment(this.activeMonth), 'month')
+				});
+				thisMonth = thisMonth.add(1, 'months');
+			}
+
+			return monthsOut;
+		}
+	},
+	filters: {
+		
+	}
+});
---
+++ content/Components/Budget.tpl
@@ -0,0 +1,17 @@
+﻿<div class="layout-container">
+
+    <div id="layout-navbar" class="navbar navbar-expand-lg align-items-lg-center bg-white">
+        <div class="navbar-nav align-items-lg">
+            <h2>Budget</h2>
+        </div>
+
+        <div class="navbar-nav align-items-lg ml-auto">
+            <button type="button" class="btn btn-outline-primary rounded-pill">Configure Jars</button>
+        </div>
+    </div>
+
+    <div class="btn-group month-bar">
+        <button type="button" class="btn btn-default" v-for="(month,index) in months" v-bind:class="{ disabled: !month.enabled, active: month.active }">{{month.shortMonth}}</button>
+    </div>    
+
+</div>
