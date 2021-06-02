889d02538765e196f602ea4929d1baca333f68e9
Ian Norris <icstatic@gmail.com>
2021-06-02 02:28:34 +0100

    Year dropdown

--- content/Components/Budget.js
+++ content/Components/Budget.js
@@ -4,10 +4,10 @@ Vue.component('jar-budget', {
 	template: '#BudgetTemplate',
 	data: function () {
 		return {
-			earliestBudgetMonth: '2019-03-03 00:00',
-			lastBudgetMonth: '2020-09-03 00:00',
-			activeMonth: '2020-07-01 00:00',
-			year: 2020,
+			earliestBudgetMonth: '2019-03-03',
+			lastBudgetMonth: '2020-09-03',
+			activeMonth: '2020-07-01',
+			activeYear: moment().year(),
 		};
 	},
 	created: async function () {
@@ -16,15 +16,35 @@ Vue.component('jar-budget', {
 	methods: {
 		getShortMonth: function (month) {
 			return month.format('MMM YYYY');
+		},
+		selectActiveYear: function (activeYear) {
+			this.activeYear = activeYear;
+			var availableMonths = this.months;
+
+			this.activeMonth = null;
+			if (availableMonths.length != 0) {
+				for (var index = availableMonths.length - 1; index >= 0; index--) {
+					if (availableMonths[index].enabled) {
+						this.activeMonth = availableMonths[index].month.format('YYYY-MM-DD');
+						break;
+					}
+				}
+			}
+			
+		},
+		selectActiveMonth: function (activeMonth) {
+			if (activeMonth.enabled) {
+				this.activeMonth = activeMonth.month.format('YYYY-MM-DD');
+			}
 		}
 	},
 	computed: {
 		months: function () {
 			var monthsOut = [];
-			var thisMonth = moment(`${this.year+1}-12-31 00:00`).startOf('year').subtract(12, 'months');
+			var thisMonth = moment(`${this.activeYear+1}-12-31 00:00`).startOf('year').subtract(12, 'months');
 			for (var months = 0; months < 12; months++) {
 				monthsOut.push({
-					month: thisMonth,
+					month: moment(thisMonth),
 					shortMonth: this.getShortMonth(thisMonth),
 					enabled: thisMonth.isBetween(moment(this.earliestBudgetMonth).startOf('month'), moment(this.lastBudgetMonth).startOf('month'), undefined, '[]'),
 					active: thisMonth.isSame(moment(this.activeMonth), 'month')
@@ -33,6 +53,17 @@ Vue.component('jar-budget', {
 			}
 
 			return monthsOut;
+		},
+		years: function () {
+			var yearsOut = [];
+			var year = moment(this.earliestBudgetMonth).startOf('year');
+			var endYear = moment(this.lastBudgetMonth).startOf('year');
+			while (year.isSameOrBefore(endYear)) {
+				yearsOut.push(year.year());
+				year = year.add(1, 'year');
+			}
+
+			return yearsOut;
 		}
 	},
 	filters: {
--- content/Components/Budget.tpl
+++ content/Components/Budget.tpl
@@ -6,12 +6,21 @@
         </div>
 
         <div class="navbar-nav align-items-lg ml-auto">
+            <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{activeYear}}</button>
+                <div class="dropdown-menu daterangepicker ltr opensleft daterangepicker-noposition dropdown-menu-right" x-placement="bottom-end">
+                    <a class="dropdown-item" v-for="(year,index) in years" v-bind:class="{ active: activeYear == year }" v-on:click="selectActiveYear(year)">{{year}}</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-nav align-items-lg ml-2">
             <button type="button" class="btn btn-outline-primary rounded-pill">Configure Jars</button>
         </div>
     </div>
 
     <div class="btn-group month-bar">
-        <button type="button" class="btn btn-default" v-for="(month,index) in months" v-bind:class="{ disabled: !month.enabled, active: month.active }">{{month.shortMonth}}</button>
+        <button type="button" class="btn btn-default" v-for="(month,index) in months" v-bind:class="{ disabled: !month.enabled, active: month.active }" v-on:click="selectActiveMonth(month)">{{month.shortMonth}}</button>
     </div>    
 
 </div>
