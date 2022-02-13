38d7bc004a876b9739e33ae781edcee10d3782e2
Ian Norris <icstatic@gmail.com>
2022-02-13 23:17:23 +0000

    Simplified startup

--- content/Scripts/App.js
+++ content/Scripts/App.js
@@ -18,22 +18,13 @@ let onLoaded = async function () {
 	globalApp = app;
 };
 
-let waitingFor = 2;
-let onReadyEvent = async function () {
-	if (--waitingFor == 0) {
-		await onLoaded();
-	}
-};
-
 $('body').on("jarTemplatesLoaded", async function (event) {
-	await onReadyEvent();
-
 	//Tell the backend to inject the API bindings
 	//This will also call onReadyEvent as well.
 	window.chrome.webview.postMessage({
 		Target: "",
 		Function: "InjectBindings",
-		Payload: "{ CallbackName: \"onReadyEvent\" }",
+		Payload: "{ CallbackName: \"onLoaded\" }",
 		Callback: 0
 	});
 });
